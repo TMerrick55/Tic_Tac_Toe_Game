e8d1dab0c01461044d4f7d93602f7a17844a27fb
TMerrick55 <tmerrick44@gmail.com>
2017-03-04 12:06:24 -0500

    Tic-Tac-Toe Ver. 8.5

---
+++ human.rb
@@ -0,0 +1,15 @@
+require_relative 'console_game.rb'
+
+class Human
+
+	attr_accessor :marker
+
+	def initialize(marker)
+		@marker = marker
+	end
+
+	def get_move(board_state)
+		print 'Enter 0-8 to choose your square: '
+		move = gets.chomp.to_i
+	end
+end
--- test_sequential.rb
+++ test_sequential.rb
@@ -12,4 +12,24 @@ class Test_Sequential_AI < Minitest::Test
 		ai = Sequential.new('O')
 		assert_equal(1, ai.get_move(['X', '', '', '', '', '', '', '', '']))
 	end
+
+	def test_AI_picks_third_pos
+		ai = Sequential.new('O')
+		assert_equal(2, ai.get_move(['X', 'X', '', '', '', '', '', '', '']))
+	end
+
+	def test_AI_picks_fourth_pos
+		ai = Sequential.new('O')
+		assert_equal(3, ai.get_move(['X', 'X', 'X', '', '', '', '', '', '']))
+	end
+
+	def test_AI_picks_fifth_pos
+		ai = Sequential.new('O')
+		assert_equal(4, ai.get_move(['X', 'X', 'X', 'X', '', '', '', '', '']))
+	end
+
+	def test_AI_picks_sixth_pos
+		ai = Sequential.new('O')
+		assert_equal(5, ai.get_move(['X', 'X', 'X', 'X', 'X', '', '', '', '']))
+	end
 end
