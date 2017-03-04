d03aba11dd0cfbbb6a7a4013f1abcd274736b36e
TMerrick55 <tmerrick44@gmail.com>
2017-03-04 13:27:49 -0500

    Tic-Tac-Toe Ver. 9.25

--- test_human.rb
+++ test_human.rb
@@ -8,15 +8,15 @@ class TestPlayer < Minitest::Test
 		assert_equal(0, player.get_move('placeholder'))
 	end
 
-	# def test_get_player_move_2
-	# 	player = Human.new('O')
-	# 	assert_equal(4, player.get_move('placeholder'))
-	# end
+	def test_get_player_move_2
+		player = Human.new('O')
+		assert_equal(4, player.get_move('placeholder'))
+	end
 
-	# def test_get_player_move_3
-	# 	player = Human.new('X')
-	# 	assert_equal(2, player.get_move('placeholder'))
-	# end
+	def test_get_player_move_3
+		player = Human.new('X')
+		assert_equal(2, player.get_move('placeholder'))
+	end
 
 	def test_get_player_move_4
 		player = Human.new('O')
