3f307e4efe8cf280349044a95e84f593c29149fd
TMerrick55 <tmerrick44@gmail.com>
2017-03-08 17:38:29 -0500

    Tic-Tac-Toe Ver. 10.5

--- unbeatable.rb
+++ unbeatable.rb
@@ -7,6 +7,57 @@ require_relative 'board.rb'
 		@marker = marker
 	end
 
+	def check_fork(ttt_board)
+		array_fork = [
+					[0, 1, 2], 
+					[3, 4, 5], 
+					[6, 7, 8], 
+					[0, 3, 6], 
+					[1, 4, 7], 
+					[2, 5, 8], 
+					[0, 4, 8], 
+					[2, 4, 6]
+					]
+					
+			fork_position =  [
+				[ttt_board[0], ttt_board[1], ttt_board[2]],
+				[ttt_board[3], ttt_board[4], ttt_board[5]],
+				[ttt_board[6], ttt_board[7], ttt_board[8]],
+				[ttt_board[0], ttt_board[3], ttt_board[6]],
+				[ttt_board[1], ttt_board[4], ttt_board[7]],
+				[ttt_board[2], ttt_board[5], ttt_board[8]],
+				[ttt_board[0], ttt_board[4], ttt_board[8]],
+				[ttt_board[2], ttt_board[4], ttt_board[6]]
+								]
+
+			key = []
+			fork_position.each_with_index do |combo_fork, index_fork|
+				if combo_fork.count(marker) == 1 && combo_fork.count('') == 2
+					key << index_fork
+				end
+			end
+
+			fork_squared = []
+			key.each do |index|
+				fork_squared << array_fork[index]
+			end
+
+			array_results = []
+			fork_squared = fork_squared.flatten.sort
+			fork_squared.each do |square|
+				if ttt_board[square] == ''
+					array_results << square
+				end
+			end
+
+			if array_results.find {|combo| array_results.count(combo) > 1} == nil
+				move = 9
+			else
+				move = array_results.find {|combo| array_results.count(combo) > 1}
+			end
+			move
+	end
+
 	def get_move(ttt_board)
 		winning_pos = [
 					[0, 1, 2], 
@@ -18,7 +69,7 @@ require_relative 'board.rb'
 					[0, 4, 8], 
 					[2, 4, 6]
 					]
-		# win_array.each do |win|
+
 			board_position =  [
 				[ttt_board[0], ttt_board[1], ttt_board[2]],
 				[ttt_board[3], ttt_board[4], ttt_board[5]],
