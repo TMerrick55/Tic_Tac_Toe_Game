c1e1335832eff0dbad8cbc0f66eba8f6d2fafa44
TMerrick55 <tmerrick44@gmail.com>
2017-03-16 12:14:22 -0400

    Tic-Tac-Toe App Ver. 0.80- UNSURE YET IF WORKING OR NOT

--- Play_Console_Game.rb
+++ Play_Console_Game.rb
@@ -5,8 +5,8 @@ require_relative 'console_game.rb'
 
 # game=Console_Game.new(Random.new('X'), Human.new('O'))
 # game=Console_Game.new(Random.new('X'), Random.new('O'))
-# game=Console_Game.new(Human.new('X'), Human.new('O'))
-game=Console_Game.new(Unbeatable.new('X'), Unbeatable.new('O'))
+game=Console_Game.new(Human.new('X'), Human.new('O'))
+# game=Console_Game.new(Unbeatable.new('X'), Unbeatable.new('O'))
 
 
 game.intro_ttt
